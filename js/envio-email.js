ba0e6502a7663918ab4566ce41932108840bf80f
Fabio R Fonseca <ffonseca1985@gmail.com>
2022-05-05 16:25:22 -0300

    ajustments

--- js/envio-email.js
+++ js/envio-email.js
@@ -25,14 +25,14 @@ $("#btnEnvio").on("click", function(event) {
     var url = "https://s463wy47f4.execute-api.us-east-1.amazonaws.com/default/sendemail/";
 
     var post = $.post(url, JSON.stringify(json), function(){
-        alert("Obrigado por escolher nossos serviços - Em breve entraremos em contato!")
+        alert("Obrigado por escolher nossos servicos - Em breve entraremos em contato!")
         $("#btnEnvio").val("Enviar");
         limparDados();
     });
 
     post.fail(function(ex){
         console.log(ex)
-        alert("Obrigado por escolher nossos serviços - Em breve entraremos em contato!")
+        alert("Obrigado por escolher nossos servicos - Em breve entraremos em contato!")
         $("#btnEnvio").val("Enviar");
         limparDados();
     });
